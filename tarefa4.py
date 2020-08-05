fed060544ca2554751ea8ae9fb85ce47fbf22993
Andre Dias <ardias2008@gmail.com>
2020-08-05 14:35:31 -0300

    alteraçoes feitas

--- tarefa4.py
+++ tarefa4.py
@@ -1,18 +1,17 @@
-<<<<<<< HEAD
-letra = input("Digite uma letra: ")
+#<<<<<<< HEAD
+#etra = input("Digite uma letra: ")
 
 #if letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u"\
 #        or letra == "A" or letra == "E" or letra == "I" or letra == "O" or letra == "U":
-if "aeiou".find(letra.lower()) >= 0:
-    print("vogal")
-else:
-=======
+#if "aeiou".find(letra.lower()) >= 0:
+#    print("vogal")
+#else:
+#=======
 letra = input("Digite uma letra: ")
 
-#if letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u"\
-#        or letra == "A" or letra == "E" or letra == "I" or letra == "O" or letra == "U":
-if "aeiou".find(letra.lower()) >= 0:
+if letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u"\
+       or letra == "A" or letra == "E" or letra == "I" or letra == "O" or letra == "U":
+#if "aeiou".find(letra.lower()) >= 0:
     print("vogal")
 else:
->>>>>>> 283b0bc2ac016cc337ecfbebc53bcf24a4698b7d
     print("Consoante")
